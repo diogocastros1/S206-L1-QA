a11af6ff1d47758213553a3d79842eff593ef6eb
diogocastros1 <diogoantonio@gec.inatel.br>
2023-04-24 22:38:43 -0300

    Aula 3 | Testes GlobalsQA
    Login, registro de usuarios e criação de funções

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # S206-L1-QA
 Repositório para entrega dos exercícios de S206 - Qualidade de Software.
+
+## Resumo
+É um boa prática criar testes independentes, por exemplo, caso tenha criado um usuario para validar o registro de usuario, não se deve utilizar este mesmo usuario para executar um teste de login, pois pode quebrar em alguma parte e ser mais moroso para encontrar a falha.
+## Cypress
+### Instalação
+~~~
+npm install cypress
+~~~
+### Executando
+~~~
+./node_modules/.bin/cypress open
+~~~
--- testes/cypress/e2e/exemplo1.cy.js
+++ testes/cypress/e2e/exemplo1.cy.js
@@ -1,5 +1,58 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// Linha para que a ide entenda o cypress
+/// <reference types="cypress"/>
+
+describe('Criando cenario de testes para o site globalsqa', () => {
+  it.skip('Caso de teste: Registrando usuarios no site com sucesso', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click() // função para clicar em um botão, para qual elemento 
+    // selecionar, basta inspecionar a pagina com o cypress
+    cy.get('#firstName').type('Inatel')
+    cy.get('#Text1').type('Tecnologias')
+    cy.get('#username').type('inatel')
+    cy.get('#password').type('tecnologias')
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should('contain.text', 'Registration successful') // contain.text -> contem este texto e algo mais
+  })
+
+  it.skip('Caso de teste: Registrando usuario com falha (faltando senha)', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register')
+    cy.get('#firstName').type('Inatel')
+    cy.get('#Text1').type('Tecnologias')
+    cy.get('#username').type('inatel')
+    cy.get('#password').type('tecnologias')
+    cy.get('#password').clear()
+    cy.get('.has-error > .help-block').should('have.text', 'Password is required') // have.text -> texto especifico
+    cy.get('.btn-primary').should('be.disabled')
+  })
+
+  it('Caso de teste: Efetuando login com sucesso', () => {
+    let info = criarUsuario()
+
+    cy.get('#username').type(info[0])
+    cy.get('#password').type(info[1])
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should('have.text', `Hi ${info[0]}!`)
   })
 })
+
+function criarUsuario() {
+  let horas = new Date().getHours().toString()
+  let minutos = new Date().getMinutes().toString()
+  let seg = new Date().getSeconds().toString()
+  let usuario = horas + minutos + seg + 'Id'
+  let senha = horas + minutos + seg + 'Senha'
+  let infoUsuario = [usuario, senha]
+
+  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+  cy.get('.btn-link').click() // função para clicar em um botão, para qual elemento 
+  // selecionar, basta inspecionar a pagina com o cypress
+  cy.get('#firstName').type(usuario)
+  cy.get('#Text1').type(usuario)
+  cy.get('#username').type(usuario)
+  cy.get('#password').type(senha)
+  cy.get('.btn-primary').click()
+  cy.get('.ng-binding').should('contain.text', 'Registration successful') // contain.text -> contem este texto e algo mais
+
+  return infoUsuario
+
+}
